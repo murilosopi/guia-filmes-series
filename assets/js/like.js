71e5baef5369f95356e084d5f41f7cdcb8ae3d09
Murilo Sopi <dev.murilosopi@gmail.com>
2022-11-07 11:06:58 -0300

    Cria like.js para formulário de review

---
+++ assets/js/like.js
@@ -0,0 +1,16 @@
+(function() {
+  const likeToggler = document.getElementById('like-toggler');
+  const likeInput = document.getElementById('like');
+  likeInput.value = 0;
+  
+  likeToggler.addEventListener('click', () => {
+    let likeIcon = likeToggler.querySelector('i');
+    if(likeInput.value == 1) {
+      likeIcon.className = "bi bi-heart";
+      likeInput.value = 0;
+    } else {
+      likeIcon.className = "bi bi-heart-fill";
+      likeInput.value = 1;
+    }
+  });
+})()
